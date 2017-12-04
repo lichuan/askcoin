f110566df26dc7b6dc13d44b3a18b402edb658dc
lichuan <308831759@qq.com>
2017-12-05 04:00:50 +0800

    update tracker script

--- generate_submodule_tracker.sh
+++ generate_submodule_tracker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+#   when any of the submodules have been changed, please execute
+# this shell script to regenerate submodule_tracker.txt to track
+# their commit ids in the root repo(askcoin)'s commit history,
+# this is useful one day when we want to reset root repo.
+
 echo "" > ./submodule_tracker.txt
-echo "  when any of the submodules have been changed, please execute" >> ./submodule_tracker.txt
-echo "this shell script to regenerate submodule_tracker.txt to track" >> ./submodule_tracker.txt
-echo "their commit ids in the root repo(askcoin)'s commit history," >> ./submodule_tracker.txt
-echo "this is useful one day when we want to reset root repo." >> ./submodule_tracker.txt
-echo "" >> ./submodule_tracker.txt
 git submodule foreach --recursive 'git log | head -n 1' >> ./submodule_tracker.txt
 echo "" >> ./submodule_tracker.txt
 git submodule foreach --recursive 'git remote -v' >> ./submodule_tracker.txt
