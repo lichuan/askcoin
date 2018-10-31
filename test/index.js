c4847e43d54e70ef3ac4356e749df48ea9f1edaf
lichuan <308831759@qq.com>
2018-11-01 00:52:23 +0800

    nodejs wsock and ecdsa

--- test/index.js
+++ test/index.js
@@ -1,105 +1,156 @@
 var hash = require('hash.js');
+const { randomBytes } = require('crypto')
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
 var WebSocket = require('ws');
 
-var ws = new WebSocket('ws://172.104.48.244:18051');
-//ws = new WebSocket('ws://192.168.0.122:18051');
+// protocol:
+// {"msg_type":2,"msg_cmd":3,"msg_id":1,"sign":"tx sign data","data":{}}
 
-ws.on('open', function open() {
-    console.log("reccv open msg");
-    ws.send(JSON.stringify({msg_type:0, msg_cmd: 2}));
-    ws.send(JSON.stringify({msg_type:0, msg_cmd: 0}));
-});
+// tx register account
+// {
+//     "sign":"oijowwwwwwwww",
+//     "data":{
+//         "type":1,
+//         "pubkey":"eeeeeeeeeee",
+//         "utc":9988888,
+//         "avatar":22,
+//         "sign":"awfwewewdffff",
+//         "sign_data": {
+//             "block_id":9283,
+//             "fee":100000000,
+//             "name":"b64(iijjjj中文)",
+//             "referrer":"pubkey......."
+//         }
+//     }
+// }
 
-ws.on('error', function(e) {
-    console.log("recv error", e);
-    //ws.send('recv open msg');
-});
+// tx sendcoin
+// {
+//     "sign":"oijowwwwwwwww",
+//     "data":{
+//         "type":2,
+//         "pubkey":"oikkkkkkkkkk",
+//         "utc": 2989883883,
+//         "block_id": 339,
+//         "fee":2000022,
+//         "amount": 10000000,
+//         "memo":"this is my memo msg",
+//         "receiver":"pubkey........"
+//     }
+// }
+
+// tx new topic
+// {
+//     "sign":"oijowwwwwwwww",
+//     "data":{
+//         "type":5,
+//         "pubkey":"oikkkkkkkkkk",
+//         "utc":9988888,
+//         "block_id": 339,
+//         "fee":2222,
+//         "reward":9999,
+//         "topic":"abceffabbbb323233232323"
+//     }
+// }
+
+// tx topic reply
+// {
+//     "sign":"oijowwwwwwwww",
+//     "data":{
+//         "type":6,
+//         "pubkey":"eeeeeeeeee",
+//         "utc":9988888,
+//         "block_id": 339,
+//         "fee":332,
+//         "topic_key":"lllllllllllllllllllllllllllllllll",
+//         "reply":"abceffabbbb323233232323",
+//         "reply_to":"to key..."
+//     }
+// }
+
+// tx give reward
+// {
+//     "sign":"oijowwwwwwwww",
+//     "data":{
+//         "type":7,
+//         "pubkey":"2fwfewefef",
+//         "utc":9988888,
+//         "block_id": 339,
+//         "fee":98,
+//         "topic_key":"lllllllllllllllllllllllllllllllll",
+//         "reply_to":"ooooooooooiwiieiweo",
+//         "amount":9222
+//     }
+// }
+
+
+// //var ws = new WebSocket('ws://172.104.48.244:18051');
+// var ws = new WebSocket('ws://192.168.0.122:19051');
+
+// ws.on('open', function open() {
+//     console.log("reccv open msg");
+//     ws.send(JSON.stringify({msg_id: 22, msg_type:0, msg_cmd: 2}));
+// });
+
+// ws.on('error', function(e) {
+//     console.log("error event....................", e);
+// });
+
+// ws.on('close', function(e) {
+//     console.log("close event....................", e);
+// });
  
-ws.on('message', function incoming(data) {
-    console.log("recv msg:");
-    console.log(data);
+// ws.on('message', function incoming(data) {
+//     console.log("recv msg:");
+//     console.log(data);
     
-});
+// });
 
-return;
-console.log('bbbbbbbbbbbbbbb');
+// return;
 
+console.log('----------------------------------------------');
 
+var tx_data = '{\"type\":1,\"pubkey\":\"BCf32BSqhVpDy04kIpC59IT16yp77oAJnEBeRJZWc7I8JtYd2HlCtUJPPKSA4yea7dyWowXpGbaRpKoGD9Wy0mk=\",\"utc\":1540950124,\"avatar\":1,\"sign\":\"MEUCIQCjZolI48Sxgn5oWcljhfgiGpfnNRWF9fth3NbLWMjdKQIgXnHzKCDS3ErPyFIkgXfXSyYxOjObnnq9y/FakU2UpaU=\",\"sign_data\":{\"block_id\":1051,\"fee\":2,\"name\":\"dHN0MQ==\",\"referrer\":\"BH6PNUv9anrjG9GekAd+nus+emyYm1ClCT0gIut1O7A3w6uRl7dAihcD8HvKh+IpOopcgQAzkYxQZ+cxT+32WdM=\"}}';
 
+var hash_raw = hash.sha256().update(hash.sha256().update(tx_data).digest()).digest();
+var hash_b64 = Buffer.from(hash_raw).toString('base64');
+console.log("hash:", hash_b64);
 
+var privkey_buf = Buffer.from("Vm1wSmQwMVhSWGxUYTJScVUwWktXRmxzVWtKaVJUQjN=", 'base64');
+var privkey = ec.keyFromPrivate(privkey_buf);
+var sign = privkey.sign(hash_raw).toDER();
+var sign_b64 = Buffer.from(sign).toString('base64');
+console.log("sign:", sign_b64);
 
+var pubkey_hex = privkey.getPublic('hex');
+var pubkey_b64 = Buffer.from(pubkey_hex, 'hex').toString('base64');
+console.log("pubkey:", pubkey_b64);
 
-while(true) {
-    var key = ec.genKeyPair();
-    var privstr = key.priv.toString(16, 2);
-    var priv2 = key.getPrivate("hex");
-    console.log(privstr, privstr.length, typeof privstr);
-    console.log(priv2, priv2.length, typeof priv2);
-    console.log(key, typeof key);
+var pubkey = ec.keyFromPublic(pubkey_hex, 'hex');
 
-
-    var res = hash.sha256().update('a1232323232342342bc').digest();
-    // var res = hash.ripemd160().update(hash.sha256().update('a1232323232342342bc').digest()).digest('hex');
-    // var res_256 = hash.sha256().update(hash.sha256().update('a1232323232342342bc').digest()).digest('hex');
-    // console.log("typeof res: ", typeof res, " typeof res_256: ", typeof res_256);
-    // console.log(res,res_256);
-    // console.log('hash:', res, typeof res, res.length);
-    // console.log('hash_256:', res_256, typeof res_256, res_256.length);
-
-
-    var sig = key.sign(res);
-    console.log('sig: ', sig, typeof sig);
-    sig = sig.toDER();
-    console.log('sig der:', sig, typeof sig, sig.length);
-    console.log(key.verify(res, sig), typeof sig);
-
-    console.log("pub.....................................");
-    var pub = key.getPublic('hex');
-    console.log(pub, typeof pub, pub.length);
-
-    var key = ec.keyFromPublic(pub, 'hex');
-    console.log("pub end.............................");
-
-    console.log(key.verify(res, sig));
-
-    if(privstr.startsWith('0')) {
-        break;
-    }
-
-
-
-    var bf = new Buffer(pub, 'hex');
-    //console.log("buf: ", bf);
-    var lcres = hash.sha256().update(bf).digest();
-    console.log("lcres: ", lcres, typeof lcres);
-    var bf2 = new Buffer(res);
-    console.log("lcres2: ", bf2.toString('base64'));
-    
-    break;
+if(pubkey.verify(hash_raw, sign))
+{
+    console.log("verify sign successfully");
 }
 
-var ec = new EC('secp256k1');
-var pub2 = "04889eedad4977924cce46baac49e6e58bac17cf7a6eca04da051fcf81cfa896cf093f11c13ff22071b6a58b514df904ce00510f1232c9ba942c0e4218324d8afc";
-var pub2_buf = Buffer(pub2, 'hex').toString('base64');
-console.log("pub2_buf: ", pub2_buf);
+// generate
+var key_pair = ec.genKeyPair();
+var privkey_hex = key_pair.getPrivate("hex");
+var privkey = ec.keyFromPrivate(privkey_hex, 'hex');
+var pubkey_hex = privkey.getPublic('hex');
+var pubkey = ec.keyFromPublic(pubkey_hex, 'hex');
+var privkey_b64 = Buffer.from(privkey_hex, 'hex').toString('base64');
+var pubkey_b64 = Buffer.from(pubkey_hex, 'hex').toString('base64');
+console.log("generate new privkey:", privkey_b64);
+console.log("generate new pubkey:", pubkey_b64);
 
-privstr = 'd5c7719843f730eb4712bad2d6b60e4315b39affdf814b27ffe86f95e808304';
-var key2 = ec.keyFromPrivate(privstr, 'hex');
-console.log(key2, typeof key2);
-var sig = key2.sign(res);
-sig = sig.toDER();
-console.log('sig2: ', sig, typeof sig, sig.length);
-var sig2_buf = new Buffer(sig).toString('base64');
-console.log("sig2_buf: ", sig2_buf, sig2_buf.length);
+// test new privkey and pubkey
+var sign = privkey.sign(hash_raw).toDER();
+var sign_b64 = Buffer.from(sign).toString('base64');
+console.log("sign:", sign_b64);
 
-//console.log('sig2 der: ', sig, typeof sig, sig.length);
-console.log(key2.verify(res, sig));
-
-var pub = key2.getPublic('hex');
-console.log(pub, typeof pub);
-
-var key = ec.keyFromPublic(pub, 'hex');
-console.log(key.verify(res, sig));
-
+if(pubkey.verify(hash_raw, sign))
+{
+    console.log("new pubkey verify sign successfully");
+}
